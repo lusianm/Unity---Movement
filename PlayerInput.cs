6966a8ffe1e26921648a6b33783a2b78d5036d66
lusianm <47287650+lusianm@users.noreply.github.com>
2019-09-20 15:24:57 +0900

    Add script

---
+++ PlayerInput.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+// 플레이어 캐릭터를 조작하기 위한 사용자 입력을 감지
+// 감지된 입력값을 다른 컴포넌트들이 사용할 수 있도록 제공
+public class PlayerInput : MonoBehaviour
+{
+    public string jumpButtonName = "Jump";
+    public string moveHorizontalAxisName = "Horizontal"; // 좌우 회전을 위한 입력축 이름
+    public string moveVerticalAxisName = "Vertical"; // 앞뒤 움직임을 위한 입력축 이름
+
+    // 값 할당은 내부에서만 가능
+
+    public Vector2 moveInput { get; private set; }
+    public bool jump { get; private set; }
+
+
+    // 매프레임 사용자 입력을 감지
+    private void Update()
+    {
+        moveInput = new Vector2(Input.GetAxis(moveHorizontalAxisName), Input.GetAxis(moveVerticalAxisName));
+        if (moveInput.sqrMagnitude > 1) moveInput = moveInput.normalized;
+
+        jump = Input.GetButtonDown(jumpButtonName);
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController characterController;
+    private PlayerInput playerInput;
+    private Animator animator;
+    
+    private Camera followCam;
+    
+    public float speed = 6f;
+    public float jumpVelocity = 20f;
+    [Range(0.01f, 1f)] public float airControlPercent;
+
+    private float speedSmoothVelocity;
+    public float speedSmoothTime = 0.1f;
+
+    private float turnSmoothVelocity;
+    public float turnSmoothTime = 0.1f;
+    
+    
+    private float currentVelocityY;
+    
+    public float currentSpeed =>
+        new Vector2(characterController.velocity.x, characterController.velocity.z).magnitude;
+    
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        playerInput = GetComponent<PlayerInput>();
+        followCam = Camera.main;
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate()
+    {
+        //제자리에서 시점 변경시 회전하지 않도록 하기 위해서 해당 상황에서만 캐릭터 회전
+        if (currentSpeed > 0.2f) Rotate();
+
+        Move(playerInput.moveInput);
+        
+        if (playerInput.jump) Jump();
+    }
+
+    private void Update()
+    {
+        UpdateAnimation(playerInput.moveInput);
+    }
+
+    public void Move(Vector2 moveInput)
+    {
+        var targetSpeed = speed * moveInput.magnitude;
+        //moveInput.y : Vertical 이동값, moveInput.x : Horizontal 이동값
+        var moveDirection = Vector3.Normalize(followCam.transform.forward * moveInput.y + followCam.transform.right * moveInput.x);
+        
+        //공중에서와 지상에서의 이동 차이점
+        var smoothTime = characterController.isGrounded ? speedSmoothTime : speedSmoothTime / airControlPercent;
+
+        if (characterController.isGrounded) currentVelocityY = 0;
+        else currentVelocityY += Time.deltaTime * Physics.gravity.y;
+
+        var velocity = moveDirection * targetSpeed + Vector3.up * currentVelocityY;
+
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    public void Rotate()
+    {
+        var targetRotation = followCam.transform.eulerAngles.y;
+
+        transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation,
+                                    ref turnSmoothVelocity, turnSmoothTime);
+    }
+
+    /*
+    2번째 회전 방식
+    public void Rotate(Vector3 direction)
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), turnSmoothTime);
+    }
+    */
+
+    public void Jump()
+    {
+        if (!characterController.isGrounded) return;
+        currentVelocityY = jumpVelocity;
+        animator.SetTrigger("Jump");
+    }
+
+    
+    private void UpdateAnimation(Vector2 moveInput)
+    {
+        var animationSpeedPercent = currentSpeed / speed;
+        Debug.Log(currentSpeed);
+
+        animator.SetFloat("speed", animationSpeedPercent);
+        animator.SetFloat("Horizontal Move", moveInput.x * animationSpeedPercent, 0.05f, Time.deltaTime);
+        animator.SetFloat("Vertical Move", moveInput.y * animationSpeedPercent, 0.05f, Time.deltaTime);
+    }
+    
+}
